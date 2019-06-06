c55c5b188d59dbebdb6ab9239766baf49a84bd9e
Juan Carlo Banayo <johncban@gmail.com>
2019-06-06 02:08:08 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,7 +2,7 @@ require 'open-uri'
 require 'pry'
 
 class Scraper
-  binding.pry
+  # binding.pry
   def self.scrape_index_page(index_url)
     in_page = Nokogiri::HTML(open(index_url))
     students = []
