f496105d02ebd9142aa45d1bfa2fdc3fa293f8e5
Juan Carlo Banayo <johncban@gmail.com>
2019-06-06 03:32:23 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,7 +12,9 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-
+    create_from_collection.each{|student_hash|
+      Student.new(student_hash)
+    }
   end
 
   def add_student_attributes(attributes_hash)
