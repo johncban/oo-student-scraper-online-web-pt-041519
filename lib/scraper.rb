f302a46391a559f3264f4d8057bc6077c5bcd5a7
Juan Carlo Banayo <johncban@gmail.com>
2019-06-06 02:36:08 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,15 +2,15 @@ require 'open-uri'
 require 'pry'
 
 class Scraper
-  # binding.pry
+
   def self.scrape_index_page(index_url)
-    in_page = Nokogiri::HTML(open(index_url))
+    i_page = Nokogiri::HTML(open(index_url))
     students = []
-    in_page.css("div.roster-cards-container").each { |card|
-        card.css(".student-card").each { |student|
-            student_profile_link = "#{student.attr('href')}"
-            student_location = student.css('.student-location').text
-            student_name = student.css('.student-name').text
+    i_page.css("div.roster-cards-container").each{|c|
+        c.css(".student-card a").each{|s|
+            student_profile_link = "#{s.attr('href')}"
+            student_location = s.css('.student-location').text
+            student_name = s.css('.student-name').text
             students << {name: student_name, location: student_location, profile_url: student_profile_link}
         }
     }
@@ -19,7 +19,25 @@ class Scraper
 
   def self.scrape_profile_page(profile_url)
     student = {}
-    # profile_page = 
+    profile_page = Nokogiri::HTML(open(profile_slug))
+    l = profile_page.css(".social-icon-container").children.css("a").collect{|el|                 el.attribute('href').value
+    }
+      l.each {|li|
+        if li.include?("linkedin")
+            student[:linkedin] = li
+        elsif li.include?("github")
+            student[:github] = li
+        elsif li.include?("twitter")
+            student[:twitter] = li
+        else
+            student[:blog] = li
+        end
+      }
   end
+
+  student[:profile_quote] = profile_page.css(".profile-quote").text if profile_page.css(".profile-quote")
+  student[:bio] = profile_page.css("div.bio-content.content-holder div.description-holder p").text if profile_page.css("div.bio-content.content-holder div.description-holder p")
+
+  student
 
 end
